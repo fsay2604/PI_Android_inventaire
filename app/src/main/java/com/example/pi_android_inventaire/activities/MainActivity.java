0b0ac869535761bd5591ce47e63b96f8ec09678b
François Charles Hébert <hebert.francois.charles@gmail.com>
2021-04-29 10:54:29 -0400

    DB dans PIAndroidInventaire avec methode getDatabaseInstance()

--- app/src/main/java/com/example/pi_android_inventaire/activities/MainActivity.java
+++ app/src/main/java/com/example/pi_android_inventaire/activities/MainActivity.java
@@ -42,23 +42,19 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private static SQLiteDatabase mydb;
 
     // Boutons du menu
     private  Button btn_produit;
     private  Button btn_reservation;
     private  Button btn_compte;
-    private Button btn_connexion;
-    private Button btn_inscription;
+    private  Button btn_connexion;
+    private  Button btn_inscription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("Accueil");
-
-        // Database
-        setupDBConnection();
 
         // TEST DU API CALLER
         ApiCaller apiCaller = new ApiCaller(PIAndroidInventaire.executorService);
@@ -71,19 +67,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         // Menu
         setupMenu();
-    }
-
-    private void setupDBConnection() {
-        mydb = openOrCreateDatabase("pi_inventaire_android",MODE_PRIVATE,null);
-        //mydb.execSQL("DROP TABLE produit");
-        mydb.execSQL("CREATE TABLE IF NOT EXISTS produit(id INTEGER PRIMARY KEY,categorie_id INTEGER NOT NULL,nom VARCHAR NOT NULL,description VARCHAR,commentaire VARCHAR,qte_disponible INTEGER NOT NULL,qte_reserve INTEGER NOT NULL,qte_defectueux INTEGER NOT NULL,image VARCHAR,FOREIGN KEY(categorie_id) REFERENCES categorie(id))");
-        mydb.execSQL("CREATE TABLE IF NOT EXISTS categorie(id INTEGER PRIMARY KEY,nom VARCHAR)");
-        //mydb.execSQL("DROP TABLE reservation");
-        mydb.execSQL("CREATE TABLE IF NOT EXISTS reservation(id INTEGER PRIMARY KEY, etat_reservation_id INTEGER NOT NULL,produit_id INTEGER NOT NULL,numero_utilisateur_id INTEGER NOT NULL, date_retour_prevue varchar NOT NULL, quantite INTEGER NOT NULL, date_retour_reel VARCHAR, FOREIGN KEY(etat_reservation_id) REFERENCES type_rapport(id),FOREIGN KEY(produit_id) REFERENCES produit(id))");
-        mydb.execSQL("CREATE TABLE IF NOT EXISTS etat_reservation(id INTEGER PRIMARY KEY, libelle VARCHAR NOT NULL)");
-        //mydb.execSQL("DROP TABLE rapport");
-        mydb.execSQL("CREATE TABLE IF NOT EXISTS rapport(id INTEGER PRIMARY KEY,produit_id INTEGER NOT NULL,user_id INTEGER NOT NULL,type_rapport_id INTEGER NOT NULL,description VARCHAR NOT NULL,FOREIGN KEY(produit_id) REFERENCES produit(id),FOREIGN KEY(type_rapport_id) REFERENCES type_rapport(id))");
-        mydb.execSQL("CREATE TABLE IF NOT EXISTS type_rapport(id INTEGER PRIMARY KEY,type VARCHAR NOT NULL)");
     }
 
 
